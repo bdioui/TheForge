20198816c64a7465e8a889fd0b9644975448ae69
Bdioui Pierre <bdiouipierre@gmail.com>
2023-12-22 23:22:28 +0100

    updating static dirs

--- TheForgeWebsite/TheForgeWebsite/settings.py
+++ TheForgeWebsite/TheForgeWebsite/settings.py
@@ -120,7 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'media'),
 ]
 
 STATIC_URL = '/static/'
