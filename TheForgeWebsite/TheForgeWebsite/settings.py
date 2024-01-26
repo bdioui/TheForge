5cd777944cfebddd5ea476bc7a61404e438253c5
Bdioui Pierre <bdiouipierre@gmail.com>
2024-01-26 08:18:16 +0100

    change of blog posts links

--- TheForgeWebsite/TheForgeWebsite/settings.py
+++ TheForgeWebsite/TheForgeWebsite/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["193.70.90.13", "theforge.fr", "www.theforge.fr"]
 
